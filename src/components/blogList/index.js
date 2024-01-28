f13ddc0818284042361d2284d4acf838dc0b66fc
AndreiLhu <leahu.andreilucian@gmail.com>
2024-01-28 16:19:40 +0100

    blog image added

--- src/components/blogList/index.js
+++ src/components/blogList/index.js
@@ -1,5 +1,14 @@
 import useSWR from 'swr';
 import Link from 'next/link';
+import Image from 'next/image';
+import blogImage from '../../../public/blogImage.jpg';
+import styled from 'styled-components';
+
+const BlogImage = styled(Image)`
+  width: 400px;
+  height: auto;
+`;
+
 const fetcher = (url) => fetch(url).then((response) => response.json());
 export default function BlogList() {
   const { data, isLoading } = useSWR('/api/blogs', fetcher);
@@ -17,6 +26,7 @@ export default function BlogList() {
       {data.map((blog) => (
         <li key={blog._id}>
           <Link href={`/${blog._id}`}>{blog.title}</Link>
+          <BlogImage src={blogImage} alt={`Blog image`} />
           <h3>{blog.content}</h3>
         </li>
       ))}
